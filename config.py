2ca51d0e8d3fe2b68739cd16df2072fc2d962cc8
Agolw <83354070+Agolw@users.noreply.github.com>
2025-05-20 09:42:54 +0800

    Add files via upload
    weights

---
+++ config.py
@@ -0,0 +1,95 @@
+# claude, deepseek, meta
+Store_weight_capability = {
+        'Perception Capability': 0.195, 
+        'Analytical Capability': 0.1625,
+        'Decision-Making Capability': 0.14625,
+        'Execution Capability': 0.0975,
+        'Evolution Capability': 0.04875,
+        'Environmental Adaptation': 0.14,
+        'Sensor Integration': 0.1225,
+        'Human-Machine Interaction Maturity': 0.0875,
+    }
+Store_weight_adoption = {
+        'Modification Difficulty': 0.1999, 
+        'Scene Transfer Difficulty': 0.1333, 
+        'Technical Absorption Capacity': 0.1166,
+        'Digital Infrastructure': 0.111,
+        'Change Management Capability': .1057,
+        'Upstream-Downstream Ecosystem': .094026,
+        'Standards Completeness': .072326,
+        'Policy Compatibility': .050626,
+        'Value Chain Optimization': .013526,}
+
+Store_weight_utility = {
+        'Quality Improvement': .167276, 
+        'Cost Reduction': .203126, 
+        'Efficiency Enhancement': .17925,
+        'Risk Prevention': .119739,
+        'Value Density Coefficient': .047609,
+        'Environmental Metrics': .122624,
+        'Social Metrics': .094324,
+        'Governance Metrics': .066052,}
+
+# gpt, deepseek, meta
+Store_weight_capability_g = {
+        'Perception Capability': .188889 , 
+        'Analytical Capability': .166667,
+        'Decision-Making Capability': .144444,
+        'Execution Capability': 0.111111,
+        'Evolution Capability': 0.055556,
+        'Environmental Adaptation': 0.133333,
+        'Sensor Integration': 0.111111,
+        'Human-Machine Interaction Maturity': 0.088889,
+    }
+Store_weight_adoption_g = {
+        'Modification Difficulty': 0.179444, 
+        'Scene Transfer Difficulty': 0.137222, 
+        'Technical Absorption Capacity': 0.1025,
+        'Digital Infrastructure': 0.108194,
+        'Change Management Capability': .1025,
+        'Upstream-Downstream Ecosystem': .104167,
+        'Standards Completeness': .091667,
+        'Policy Compatibility': .054167,
+        'Value Chain Optimization': .035139,}
+
+Store_weight_utility_g = {
+        'Quality Improvement': .152873, 
+        'Cost Reduction': .199911, 
+        'Efficiency Enhancement': .176392,
+        'Risk Prevention': .117594,
+        'Value Density Coefficient': .058797,
+        'Environmental Metrics': .147217,
+        'Social Metrics': .08833,
+        'Governance Metrics': .058887,}
+
+# gpt, deepseek, meta
+PV_weight_capability = {
+        'Perception Capability': .184497, 
+        'Analytical Capability': .165654,
+        'Decision-Making Capability': 0.161926,
+        'Execution Capability': 0.116717,
+        'Evolution Capability': 0.049005,
+        'Environmental Adaptation': 0.121727,
+        'Sensor Integration': .112770,
+        'Human-Machine Interaction Maturity': 0.087703,
+    }
+PV_weight_adoption = {
+        'Modification Difficulty': 0.154086, 
+        'Scene Transfer Difficulty': 0.117814, 
+        'Technical Absorption Capacity': 0.126594,
+        'Digital Infrastructure': 0.110752,
+        'Change Management Capability': .118655,
+        'Upstream-Downstream Ecosystem': .122674,
+        'Standards Completeness': .112163,
+        'Policy Compatibility': .068963,
+        'Value Chain Optimization': .068300,}
+        
+PV_weight_utility = {
+        'Quality Improvement': .132307, 
+        'Cost Reduction': .182328, 
+        'Efficiency Enhancement': .161384,
+        'Risk Prevention': .145253,
+        'Value Density Coefficient': .056529,
+        'Environmental Metrics': .171829,
+        'Social Metrics': .100236,
+        'Governance Metrics': .050134,}
